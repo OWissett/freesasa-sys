523eae5b92365b726756d59795ed7d3b861d68ef
Oliver Wissett <wissettol@gmail.com>
2023-02-03 17:09:17 +0000

    added c++ linking

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,16 @@
 //! Please contact me via email, ow257@cam.ac.uk, if you are interested in access to this library.
 //!
 //!
-
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(unused)]
 #![allow(clippy::upper_case_acronyms)]
+
+// This is needed for ensuring that
+// we can link to libc++.
+extern crate link_cplusplus;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
