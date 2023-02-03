af7c92d72a72303c2a7c3044cedf8ac34d018714
Oliver Wissett <wissettol@gmail.com>
2023-02-03 15:07:26 +0000

    tidying

--- build.rs
+++ build.rs
@@ -1,15 +1,27 @@
 extern crate autotools;
 extern crate bindgen;
 
+use fs_extra::dir;
+
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
+    // copy freesasa to OUT_DIR
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let freesasa_dir = PathBuf::from(&out_dir).join("freesasa");
+    let freesasa_src = PathBuf::from("freesasa");
+
+    let mut copy_options = dir::CopyOptions::new();
+    copy_options.overwrite = true;
+
+    dir::copy(&freesasa_src, &out_dir, &copy_options).unwrap();
+
     // Use autotools to compile the native library
-    let dst = autotools::Config::new("freesasa")
-        .reconf("-is")
+    let dst = autotools::Config::new(&freesasa_dir)
+        .reconf("-i")
         .config_option("disable-json", None)
         .config_option("disable-xml", None)
         .build();
@@ -37,4 +49,7 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    // Delete freesasa directory
+    dir::remove(&freesasa_dir).unwrap();
 }
